3c516ac789a7f2b359b1758eb4c1ce94bc1ba540
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-07-05 21:44:13 -0300

    Refatorando o código - add classes

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: const <Widget>[
-            Card(
-                child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('100.0'),
-              subtitle: Text('1000'),
-            )),
-            Card(
-                child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('200.0'),
-              subtitle: Text('2000'),
-            )),
-          ],
-        ),
+        body: ListaTransferencias(),
         appBar: AppBar(
           title: const Text('Transferências'),
         ),
@@ -27,3 +12,39 @@ void main() => runApp(MaterialApp(
         ),
       ),
     ));
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(100.0, 1000)),
+        ItemTransferencia(Transferencia(200.0, 1000)),
+        ItemTransferencia(Transferencia(300.0, 1000)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+      leading: Icon(Icons.monetization_on),
+      title: Text(this._transferencia.valor.toString()),
+      subtitle: Text(this._transferencia.numeroConta.toString()),
+    ));
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
